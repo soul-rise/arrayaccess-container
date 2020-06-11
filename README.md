438eaed5ef4a257eebb6fde9189f8c0a83dd587c
dkrasnorutsky <dkrasnorutsky@team.amocrm.com>
2020-06-12 01:35:38 +0300

    - Add ArrayAccess adapter for \Laminas\ServiceManager\ServiceManager

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# servicemanager-array-access
-ArrayAccess adapter for laminas/laminas-servicemanager
+# arrayaccess-container
+
+What is it?
+-----------
+ArrayAccess adapter
+
+You can use your container as array
+
+You can use this adapter for example for library [davedevelopment/phpmig][phpmig]
+or you are like array
+
+P.S
+
+Also supported PSR 11
+
+Supported Containers: 
+* [laminas/laminas-servicemanager][servicemanager]
+
+Getting Started
+---------------
+```php
+use \SoulRise\ArrayAccess\LaminasServiceManager\ArrayAccessContainerAdapter;
+
+$arrayAccess = new ContainerArrayAccessAdapter($container);
+$service = $arrayAccess['your_service']; // create and get instance your service 
+```
+
+[phpmig]:https://github.com/davedevelopment/phpmig
+[servicemanager]:https://github.com/laminas/laminas-servicemanager
---
+++ src/LaminasServiceManager/ArrayAccessContainerAdapter.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace SoulRise\ArrayAccess\LaminasServiceManager;
+
+use ArrayAccess;
+use Laminas\ServiceManager\ServiceManager;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class ArrayAccessAdapter
+ *
+ * @package SoulRise\ServiceManger\Adapter
+ */
+class ArrayAccessContainerAdapter implements ContainerInterface, ArrayAccess
+{
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
+
+    /**
+     * ArrayAccessAdapter constructor.
+     *
+     * @param ServiceManager $serviceManager
+     */
+    public function __construct(ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+    }
+
+    /**
+     * Finds an entry of the container by its identifier and returns it
+     * @param string $id
+     *
+     * @return mixed|object
+     */
+    public function get($id)
+    {
+        return $this->serviceManager->get($id);
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     * @param string $id
+     *
+     * @return bool
+     */
+    public function has($id)
+    {
+        return $this->serviceManager->has($id);
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * @param mixed $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return $this->serviceManager->has($offset);
+    }
+
+    /**
+     * Finds an entry of the container by its identifier and returns it
+     *
+     * @param mixed $offset
+     *
+     * @return mixed|object
+     */
+    public function offsetGet($offset)
+    {
+        return $this->serviceManager->get($offset);
+    }
+
+    /**
+     * Set Service
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        return $this->serviceManager->setService($offset, $value);
+    }
+
+    /**
+     * Unsupported
+     *
+     * @deprecated
+     * @param mixed $offset
+     */
+    public function offsetUnset($offset)
+    {
+        // noop
+    }
+}
